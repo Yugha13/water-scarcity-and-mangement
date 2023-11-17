85240f04159cca872386ab80291fcc9b77ce1bdd
Ekshin683 <149076668+Ekshin683@users.noreply.github.com>
2023-11-17 17:23:52 +0530

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,91 @@
+function showoverview() {
+	document.getElementById('content').innerHTML = `
+	<h2>Overview</h2>
+	<p>Water scarcity has many definitions but they have one thing in common, which is an excess of water demand over the available supply (Damkjaer and Taylor 2017). The United Nations Water succinctly define it as the point at which the aggregate impact of all users impinges on the supply or quality of water under prevailing institutional arrangements to the extent that the demand by all sectors, including the environment, cannot be satisfied fully, a relative concept that can occur at any level of supply or demand (United Nations Water 2006). The Food and Agriculture Organization of the United Nations (FAO) lauded this definition recognizing that water scarcity can occur at any level of supply and demand, that it has various causes, and that it is capable of being remedied or alleviated to a certain extent (FAO 2012). Water scarcity means there is not enough water to meet the needs of everyone, including environmental flows (Water and Development Research Group 2020a)</p>
+    <img src="https://image.slideserve.com/1265852/overview-l.jpg" alt="Overview Image">
+	<p>Fifty years ago the world had fewer than half as many people as it has today. They were not 
+as wealthy. They consumed fewer calories, ate less meat, and thus required less water to 
+produce their food. The pressure they inflicted on the environment was lower. They took 
+from our rivers a third of the water that we take now.
+Today the competition for scarce water resources in many places is intense. Many 
+river basins do not have enough water to meet all the demands—or even enough for their 
+rivers to reach the sea. Further appropriation of water for human use is not possible because limits have been reached and in many cases breached. Basins are effectively “closed,” 
+with no possibility of using more water. The lack of water is thus a constraint to producing 
+food for hundreds of millions of people</p>
+	`;
+}
+function showcauses() {
+	document.getElementById('content').innerHTML = `
+	<h2>Causes</h2>
+	<p>Droughts and Climate Change
+Water shortages occur due to a number of factors; one of the biggest drivers of water scarcity is drought. Drought is a natural phenomenon in which dry conditions and lack of precipitation – whether it is rain, snow or sleet –  occur over certain areas for a period of time. 
+
+While the amount of rainfall can naturally vary between different regions and times of year, climate change and rising global temperatures are altering rainfall patterns, which in turn, impact the quality and spatial distribution of global water resources. Warmer temperatures mean that moisture in soil evaporates at faster rates, and more frequent and severe heat waves exacerbate drought conditions and contribute towards water shortages. 
+
+These conditions also create the perfect breeding ground for wildfires, further fuelling drought season and water stress. In the 2018 Intergovernmental Panel on Climate Change (IPCC) report, climate scientists say that groundwater stored in aquifers, which provides 36% of the world’s domestic water supply for over 2 billion people, is highly sensitive to future climate change. They also concluded that wet regions are expected to get wetter while dry regions will get drier. </p>
+    <img src="https://image1.slideserve.com/2394129/living-with-water-shortages-n.jpg" alt="Causes Image">
+	<p>Poor Water Management and Growing Demand
+Today, the world’s population is just short of eight billion people, which translates to a growing demand for water amid water stress from climate change. Urbanisation and an exponential increase in freshwater demand for households are both driving factors behind water shortages, especially in regions with a precarious water supply. 
+
+For example in 2018, Cape Town, South Africa experienced a water crisis and became the first modern city to effectively run out of drinking water as a result of extreme drought, poor water resource management and overconsumption. 
+
+Likewise, China is also at risk of running out of water due to poor water management. Each year, total renewable water resources per inhabitant is estimated at about 2,018 cubic metres, which is 75% less than the global average, according to the World Bank.</p>
+`;
+}
+function showeffects() {
+	document.getElementById('content').innerHTML = `
+    <h2>Effects</h2>
+	<p>As the factors driving water scarcity are complex and vary widely across countries and regions, UNICEF works at multiple levels to introduce context-specific technologies that increase access to safe water and address the impacts of water scarcity. We focus on:
+
+Identifying new water resources: We assess the availability of water resources using various technologies, including remote sensing and geophysical surveys and field investigations.
+
+Improving the efficiency of water resources: We rehabilitate urban water distribution networks and treatment systems to reduce water leakage and contamination, promoting wastewater reuse for agriculture to protect groundwater.
+
+Planning for urban scarcity: We plan for future water needs by identifying available resources to reduce the risk of cities running out of water.
+
+Expanding technologies to ensure climate resilience: We support and develop climate-resilient water sources, including the use of deeper groundwater reserves through solar-powered water networks. We also advance water storage through small-scale retention structures, managed aquifer recharge (where water is pumped into underground reserves to improve its quality), and rainwater harvesting.
+
+Changing behaviours: We work with schools and communities to promote an understanding of the value of water and the importance of its protection, including by supporting environmental clubs in schools.</p>
+<img src="https://cdn.britannica.com/73/123273-050-7B3A2B55/drought-Sonoran-Desert-Mexico.jpg" alt="Effects Image">
+<p>Planning national water needs: We work with key stakeholders at national and sub-national levels to understand the water requirements for domestic use and for health and sanitation, and advocate to ensure that this is reflected in national planning considerations.
+
+Supporting the WASH sector: We develop technical guidance, manuals and online training programmes for WASH practitioners to improve standards for water access.</p>
+`;
+}
+function showmanagement() {
+	document.getElementById('content').innerHTML = `
+	<h2>Management</h2>
+	<p>Climate change will worsen the situation by altering hydrological cycles, making water more unpredictable and increasing the frequency and intensity of floods and droughts. The roughly 1 billion people living in monsoonal basins and the 500 million people living in deltas are especially vulnerable.  Flood damages are estimated around $120 billion per year (only from property damage), and droughts pose, among others, constraints to the rural poor, highly dependent on rainfall variability for subsistence. 
+
+The fragmentation of this resource also constrains water security. There are 276 transboundary basins, shared by 148 countries, which account for 60% of the global freshwater flow. Similarly, 300 aquifers systems are transboundary in nature, with 2.5 billion people worldwide are dependent on groundwater. The challenges of fragmentation are often replicated at the national scale, meaning cooperation is needed to achieve optimal water resources management and development solutions for all riparians. To deal with these complex and interlinked water challenges, countries will need to improve the way they manage their water resources and associated services</p>
+<img src="https://www.researchgate.net/profile/John_Ward13/publication/319119034/figure/download/fig1/AS:927966116716546@1598255771879/The-water-food-energy-nexus-Source-Adapted-from-Smajgl-and-Ward-2013c.png" alt="management Image">
+<p>To strengthen water security against this backdrop of increasing demand, water scarcity, growing uncertainty, greater extremes, and fragmentation challenges, clients will need to invest in institutional strengthening, information management, and (natural and man-made) infrastructure development. Institutional tools such as legal and regulatory frameworks, water pricing, and incentives are needed to better allocate, regulate, and conserve water resources. Information systems are needed for resource monitoring, decision making under uncertainty, systems analyses, and hydro-meteorological forecast and warning. Investments in innovative technologies for enhancing productivity, conserving and protecting resources, recycling storm water and wastewater, and developing non-conventional water sources should be explored in addition to seeking opportunities for enhanced water storage, including aquifer recharge and recovery. Ensuring the rapid dissemination and appropriate adaptation or application of these advances will be a key to strengthening global water security.</p>
+`;
+}
+function showsolution() {
+	document.getElementById('content').innerHTML = `
+	<h2>Solution</h2>
+	// <p>1. Lack of access to safe, clean drinking water
+Every year 842,000 people die from diarrhoea caused by consuming unsafe drinking water or insufficient sanitation practices. Eighty percent of the illnesses in developing countries result from unhealthy water and/or sanitation systems, and one out of four deaths of children under the age of five are the result of water-related illnesses.
+
+2. Threatened ecosystems
+The rapid disappearance of wetlands reduces wildlife habitat and causes the loss of water filtration, storm protection, and flood control services typically proffered by wetlands. Collectively, the world has lost 50 percent of all its wetlands since 1900, and nowhere is immune: Even places like California, Florida, and Louisiana have witnessed high rates of wetlands disappearance.
+
+3. Unhealthy economies and increased poverty
+When water is hard to access, people must spend long periods of time collecting water. This means people are not able to attend school or work, impacting both individuals and their countries. The World Bank estimates that water scarcity can result in GDP losses of as much as 14 percent.</p>
+<img src="https://www.webuildvalue.com/wp-content/uploads/infografica-eng-siccita-c-773x1024.jpeg" alt="Solution Image">
+<p>4. Decreased food access and higher food costs
+As water becomes scarcer, it also becomes more expensive. This increases the cost of producing food crops, which then increases the cost of food in local grocery stores and markets. For example, a ten percent rise in the cost of water could increase the production cost of a single orange by as much as thirty percent. This simultaneously makes farming more difficult and decreases access to healthy food, which in turn has an impact on public health.
+
+5. Heightened conflict
+History shows that when food prices spike, so do rates of violence and social conflict. This makes sense when you consider that starving people are more willing to break with social convention if it means getting something to eat.
+
+6. Higher costs for clothing, electronics and other consumer goods
+The fashion and electronics industries are notorious for their astronomical rates of water use. As water gets more expensive, it could increase the difficulty in accessing clothing, phones, and more. Check out are articles on the impacts of water stress on industry and business to learn more.</p>
+`;
+}
+
+	
+
+
+    
