505a59aada2d1f53ab8eaeac9cefe3e10a995bcb
Ekshin683 <149076668+Ekshin683@users.noreply.github.com>
2023-11-22 22:28:22 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,4 +1,6 @@
-function showoverview() {
+
+
+	function showoverview() {
 	document.getElementById('content').innerHTML = `
 	<h2>Overview</h2>
 	<p>Water scarcity has many definitions but they have one thing in common, which is an excess of water demand over the available supply (Damkjaer and Taylor 2017). The United Nations Water succinctly define it as the point at which the aggregate impact of all users impinges on the supply or quality of water under prevailing institutional arrangements to the extent that the demand by all sectors, including the environment, cannot be satisfied fully, a relative concept that can occur at any level of supply or demand (United Nations Water 2006). The Food and Agriculture Organization of the United Nations (FAO) lauded this definition recognizing that water scarcity can occur at any level of supply and demand, that it has various causes, and that it is capable of being remedied or alleviated to a certain extent (FAO 2012). Water scarcity means there is not enough water to meet the needs of everyone, including environmental flows (Water and Development Research Group 2020a)</p>
@@ -12,7 +14,9 @@ river basins do not have enough water to meet all the demands—or even enough f
 rivers to reach the sea. Further appropriation of water for human use is not possible because limits have been reached and in many cases breached. Basins are effectively “closed,” 
 with no possibility of using more water. The lack of water is thus a constraint to producing 
 food for hundreds of millions of people</p>
+<p>youtubeLink: "https://youtu.be/E6pjj2gVnWA?si=wxzb_BL5SFWmLcRz"</p>
 	`;
+	document.getElementById('contactForm').style.display = 'none';
 }
 function showcauses() {
 	document.getElementById('content').innerHTML = `
@@ -30,6 +34,7 @@ Today, the world’s population is just short of eight billion people, which tra
 For example in 2018, Cape Town, South Africa experienced a water crisis and became the first modern city to effectively run out of drinking water as a result of extreme drought, poor water resource management and overconsumption. 
 
 Likewise, China is also at risk of running out of water due to poor water management. Each year, total renewable water resources per inhabitant is estimated at about 2,018 cubic metres, which is 75% less than the global average, according to the World Bank.</p>
+<p>youtubeLink: "https://youtu.be/hyqe8-bY6ec?si=l1LiLsICENNxCdoI"</p>
 `;
 }
 function showeffects() {
@@ -50,6 +55,7 @@ Changing behaviours: We work with schools and communities to promote an understa
 <p>Planning national water needs: We work with key stakeholders at national and sub-national levels to understand the water requirements for domestic use and for health and sanitation, and advocate to ensure that this is reflected in national planning considerations.
 
 Supporting the WASH sector: We develop technical guidance, manuals and online training programmes for WASH practitioners to improve standards for water access.</p>
+<p>youtubeLink: "https://youtu.be/E6pjj2gVnWA?si=wxzb_BL5SFWmLcRz"</p>
 `;
 }
 function showmanagement() {
@@ -60,6 +66,7 @@ function showmanagement() {
 The fragmentation of this resource also constrains water security. There are 276 transboundary basins, shared by 148 countries, which account for 60% of the global freshwater flow. Similarly, 300 aquifers systems are transboundary in nature, with 2.5 billion people worldwide are dependent on groundwater. The challenges of fragmentation are often replicated at the national scale, meaning cooperation is needed to achieve optimal water resources management and development solutions for all riparians. To deal with these complex and interlinked water challenges, countries will need to improve the way they manage their water resources and associated services</p>
 <img src="https://www.researchgate.net/profile/John_Ward13/publication/319119034/figure/download/fig1/AS:927966116716546@1598255771879/The-water-food-energy-nexus-Source-Adapted-from-Smajgl-and-Ward-2013c.png" alt="management Image">
 <p>To strengthen water security against this backdrop of increasing demand, water scarcity, growing uncertainty, greater extremes, and fragmentation challenges, clients will need to invest in institutional strengthening, information management, and (natural and man-made) infrastructure development. Institutional tools such as legal and regulatory frameworks, water pricing, and incentives are needed to better allocate, regulate, and conserve water resources. Information systems are needed for resource monitoring, decision making under uncertainty, systems analyses, and hydro-meteorological forecast and warning. Investments in innovative technologies for enhancing productivity, conserving and protecting resources, recycling storm water and wastewater, and developing non-conventional water sources should be explored in addition to seeking opportunities for enhanced water storage, including aquifer recharge and recovery. Ensuring the rapid dissemination and appropriate adaptation or application of these advances will be a key to strengthening global water security.</p>
+<p>youtubeLink: "https://youtu.be/E6pjj2gVnWA?si=wxzb_BL5SFWmLcRz"</p>
 `;
 }
 function showsolution() {
@@ -82,10 +89,22 @@ History shows that when food prices spike, so do rates of violence and social co
 
 6. Higher costs for clothing, electronics and other consumer goods
 The fashion and electronics industries are notorious for their astronomical rates of water use. As water gets more expensive, it could increase the difficulty in accessing clothing, phones, and more. Check out are articles on the impacts of water stress on industry and business to learn more.</p>
-`;
+<p> learn more <link="https://youtu.be/06XmOTrNjM0?si=18vG-2pAYp73wUng"></p>
+
+<p>youtubeLink: "https://youtu.be/WR7WsWe-z0k?si=orjADK9RsvRsltIk"</p>`;
+}
+function showcontactus() {
+	document.getElementById('content').innerHTML = `
+	<h2>Contact Us</h2>
+	<p>Contact us: +91 7331143402, +91 6303833425, +91 8712101192, +91 9541410293</p>
+	`;
 }
 
 	
+
+
+    
+
 
 
     
